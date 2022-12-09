a2f2641cb157363fb5bcb960ef4543a03bac35e2
Schismond <dadourachid720@gmail.com>
2022-12-09 15:56:19 +0100

    change writing methods

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let computer = 0;
 let funcCpt = 0;
 let floatCpt = 0;
 let clicked = false;
+let A;
+let Operator;
 //Variables
-let operations = document.getElementById("operation");
+let operation = document.getElementById("operation");
 let screen = document.getElementById("screen");
 let buttons = document.querySelectorAll('input');
 let numbers = document.getElementsByClassName('number');
@@ -12,6 +14,9 @@ let screenNum = document.getElementById("screen").innerText;
 let equal = document.getElementById('equal');
 let float = document.getElementById('point');
 //writing function
+
+/*
+
 let T=[];
 for(let j=0;j<funcBtn.length;j++){
     T[j]= funcBtn[j].value;
@@ -26,7 +31,7 @@ for(let i=0;i<funcBtn.length;i++){
             clicked = true ;
             screen.innerText += funcBtn[i].value;
             computer = computer-1;
-            funcCpt ++;
+            funcCpt++;
             console.log('cpt is'+computer);
         }else{
         screen.innerText = screen.innerText;
@@ -51,16 +56,75 @@ for(let i=0;i<numbers.length;i++){
         console.log(computer);
     });
 };
-/*
 
 float.addEventListener('click', () => {
-let i = screen.innerText.slice(-1);
-if((screen.innerText =' ' && computer == 0 ) || (T.includes(i) == true)){
+
+if(screen.innerText =='' && computer == 0 || (clicked==true) ){
     screen.innerText += '0.';
     floatCpt++;
-}
-else if( T.includes(i) == false && clicked == true && floatCpt<2 ){
+} else if( funcCpt == 1 ){
     screen.innerText += '.';
 }
 });
+
+
 */
+
+
+for(let i=0;i<numbers.length;i++){
+    numbers[i].addEventListener('click', () => {
+        //zero conditions
+        if(numbers[i].value==0 && computer == 0){
+            computer--;
+        }else if(computer<15 && computer == 0 ){
+            screen.innerText = numbers[i].value;
+        }else if(computer<15 && computer > 0 ){
+            screen.innerText += numbers[i].value;
+        }else if(computer>=15){
+            screen.innerText = screen.innerText;
+            computer--;
+        }
+        computer++;
+        console.log(computer);
+    });
+};
+
+for(let i=0;i<funcBtn.length;i++){
+    funcBtn[i].addEventListener('click', () => {
+        clicked = true ;
+        if(computer == 0){
+            screen.innerText ='';
+        }else if(computer > 0 && funcCpt == 0 ){
+            console.log('is'+computer);
+            operation.innerText += screen.innerText + funcBtn[i].value;
+            funcCpt++;
+            console.log('funcCpt=',funcCpt);
+            computer = 0;
+            A = screen.innerText;
+            console.log(A);
+            Operator = funcBtn[i].value;
+            screen.innerText = '';
+        }else if(computer > 0 && funcCpt == 1 ){
+            operation.innerText = 'calculating' + funcBtn[i].value;
+            screen.innerText = '0';
+           //calculate(A,B,Operator);
+        }else{
+        screen.innerText = screen.innerText;
+        }
+    });
+};
+
+float.addEventListener('click', () => {
+
+    if(screen.innerText =='0' && computer == 0 ){
+        screen.innerText = '0.';
+        floatCpt++;
+    } else if( computer > 0 && floatCpt == 0 ){
+        screen.innerText += '.';
+        floatCpt++;
+    } else if (computer > 0 && floatCpt == funcCpt){
+        screen.innerText += '.';
+        floatCpt++;
+    }
+    });
+    
