bcd6f06e8559c26a6596fb51362c51174d13cd52
Schismond <dadourachid720@gmail.com>
2022-12-10 15:58:10 +0100

    start calculate function (facing bugs )

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let computer = 0;
-let funcCpt = 0;
-let floatCpt = 0;
+let computer, funcCpt , floatCpt , ANS ;
+computer = funcCpt = floatCpt = ANS = 0;
 let clicked = false;
-let A;
-let Operator;
+let A,B,Operator;
+
+let funcValue;
 //Variables
 let operation = document.getElementById("operation");
 let screen = document.getElementById("screen");
@@ -14,62 +14,6 @@ let screenNum = document.getElementById("screen").innerText;
 let equal = document.getElementById('equal');
 let float = document.getElementById('point');
 //writing function
-
-/*
-
-let T=[];
-for(let j=0;j<funcBtn.length;j++){
-    T[j]= funcBtn[j].value;
-}
-
-for(let i=0;i<funcBtn.length;i++){
-    funcBtn[i].addEventListener('click', () => {
-        if(computer == 0 && funcCpt == 0){
-            screen.innerText ='';
-        } else if(computer > 0 && funcCpt == 0){
-            console.log('is'+computer);
-            clicked = true ;
-            screen.innerText += funcBtn[i].value;
-            computer = computer-1;
-            funcCpt++;
-            console.log('cpt is'+computer);
-        }else{
-        screen.innerText = screen.innerText;
-        }
-    });
-};
-
-//writing numbers
-
-for(let i=0;i<numbers.length;i++){
-    numbers[i].addEventListener('click', () => {
-        //zero conditions
-        if((numbers[i].value==0 && computer == 0) || (funcCpt ==1 && numbers[i].value==0 )){
-            computer--;
-        }else if(computer<15){
-        screen.innerText += numbers[i].value;
-        }else if(computer>=15){
-        screen.innerText = screen.innerText;
-        computer--;
-        }
-        computer++;
-        console.log(computer);
-    });
-};
-
-float.addEventListener('click', () => {
-
-if(screen.innerText =='' && computer == 0 || (clicked==true) ){
-    screen.innerText += '0.';
-    floatCpt++;
-} else if( funcCpt == 1 ){
-    screen.innerText += '.';
-}
-});
-
-
-*/
-
 
 for(let i=0;i<numbers.length;i++){
     numbers[i].addEventListener('click', () => {
@@ -93,21 +37,25 @@ for(let i=0;i<funcBtn.length;i++){
     funcBtn[i].addEventListener('click', () => {
         clicked = true ;
         if(computer == 0){
-            screen.innerText ='';
+            screen.innerText ='0';
         }else if(computer > 0 && funcCpt == 0 ){
             console.log('is'+computer);
-            operation.innerText += screen.innerText + funcBtn[i].value;
+            funcValue = funcBtn[i].value;
+            operation.innerText += screen.innerText + funcValue;
             funcCpt++;
             console.log('funcCpt=',funcCpt);
             computer = 0;
             A = screen.innerText;
-            console.log(A);
             Operator = funcBtn[i].value;
-            screen.innerText = '';
-        }else if(computer > 0 && funcCpt == 1 ){
-            operation.innerText = 'calculating' + funcBtn[i].value;
             screen.innerText = '0';
-           //calculate(A,B,Operator);
+        }else if(computer > 0 && funcCpt == 1 ){
+            B = screen.innerText;
+            //operation.innerText = ANS + funcValue;
+            screen.innerText = '0';
+            computer = 0;
+            funcCpt--;
+            floatCpt = 0;
+           calculate(A,B,Operator);
         }else{
         screen.innerText = screen.innerText;
         }
@@ -116,10 +64,12 @@ for(let i=0;i<funcBtn.length;i++){
 
 float.addEventListener('click', () => {
 
-    if(screen.innerText =='0' && computer == 0 ){
+    if( screen.innerText =='0' && computer == 0 ){
         screen.innerText = '0.';
         floatCpt++;
-    } else if( computer > 0 && floatCpt == 0 ){
+        computer++;
+    }
+    else if( computer > 0 && floatCpt == 0 ){
         screen.innerText += '.';
         floatCpt++;
     } else if (computer > 0 && floatCpt == funcCpt){
@@ -127,4 +77,29 @@ float.addEventListener('click', () => {
         floatCpt++;
     }
     });
+
+function calculate(A,B,Operator,ANS){
+    console.log('A is '+ A);
+    console.log('Operator is'+ Operator);
+    console.log('B is '+ B);
+    ANS=0;
+ 
+        
+    switch(true){
+        case Operator == '+':
+                console.log('ans value was'+ANS);
+                ANS = Number(A) + Number(B);
+                A = ANS;
+                console.log('A become '+A+'now');
+                console.log("+ answer is "+ANS);
+                operation.innerText = ANS + funcValue;
+              break;
+        case Operator == '-':
+                ANS += parseInt(A) - parseInt(B);
+                console.log("- answer is "+ANS);
+                operation.innerText =''+ ANS + funcValue;
+          break;
+    }
+        
+}
     
